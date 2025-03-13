b609f22b674cd53c8ad3da7d14c90eb0641b3716
Orochidara23000 <mada.rochi23000@gmail.com>
2025-03-13 03:29:18 +0100

    Add files via upload

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,3 +1,2 @@
-from .steam_cmd import steam_cmd
-from .downloader import download_manager
-from .game_info import game_info_service 
+from .interface import create_interface
+from . import components 
